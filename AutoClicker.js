dc01c9abcea55561d0a97bcf8e1b3149327f50b3
Teruteru <teruterubouzu1024@gmail.com>
2020-11-08 18:19:10 +0900

    Update

--- AutoClicker.js
+++ AutoClicker.js
@@ -1,56 +1,28 @@
 var AC = {};
 AC.STACK = [];
-//console.log(Game.ClickCookie.toString());
-/*Game.ClickCookie=function(event,amount)
-{
-  if (event) event.preventDefault();
-  if (Game.OnAscend || Game.AscendTimer>0) {
-    console.log('Game.CookieClick escape');
-  }
-  else
-  {
-    var amount=amount?amount:Game.computedMouseCps;
-    Game.Earn(amount);
-    Game.handmadeCookies=(Game.handmadeCookies+amount)|0;
-
-    for (var i in Game.customCookieClicks) {Game.customCookieClicks[i]();}
-      Game.cookieClicks=(Game.cookieClicks+1)|0;
-  }
-  Game.Click=0;
-};*/
-Game.ClickCookie = function (event, amount) {
-  //var now = Date.now();
-  if (event) event.preventDefault();
-  if (Game.OnAscend || Game.AscendTimer > 0) { }
+Game.ClickCookie = function (e, amount) {
+  var now = Date.now();
+  if (e) e.preventDefault();
+  if (Game.OnAscend || Game.AscendTimer > 0 || Game.T < 3) { }
   else {
-    /*
-    if (now - Game.lastClick < 1000 / 15) {
-      Game.autoclickerDetected += Game.fps;
-      if (Game.autoclickerDetected >= Game.fps * 5) Game.Win('Uncanny clicker');
-    }*/
+    Game.loseShimmeringVeil('click');
     var amount = amount ? amount : Game.computedMouseCps;
     Game.Earn(amount);
     Game.handmadeCookies += amount;
-    /*
     if (Game.prefs.particles) {
       Game.particleAdd();
       Game.particleAdd(Game.mouseX, Game.mouseY, Math.random() * 4 - 2, Math.random() * -2 - 2, Math.random() * 0.5 + 0.75, 1, 2);
     }
-    if (Game.prefs.numbers) {
-      Game.particleAdd(Game.mouseX + Math.random() * 8 - 4, Game.mouseY - 8 + Math.random() * 8 - 4, 0, -2, 1, 4, 2, '', '+' + Beautify(amount, 1));
-    }
-*/
-    for (var i in Game.customCookieClicks) {
-      Game.customCookieClicks[i]();
-    }
+    if (Game.prefs.numbers) Game.particleAdd(Game.mouseX + Math.random() * 8 - 4, Game.mouseY - 8 + Math.random() * 8 - 4, 0, -2, 1, 4, 2, '', '+' + Beautify(amount, 1));
 
-    //Game.playCookieClickSound();
+    Game.runModHook('click');
+
+    Game.playCookieClickSound();
     Game.cookieClicks++;
   }
-  //Game.lastClick = now;
-  //Game.Click = 0;
+  Game.lastClick = now;
+  Game.Click = 0;
 };
-//console.log(Game.ClickCookie.toString());
 AC.start = function start() {
   var obj = arguments[0] || {};
   var c_str = obj["clicksAtOnce"] || "1000";
@@ -88,12 +60,11 @@ AC.stop = function stop() {
   }
 };
 Game.lastClick = 0;
-setInterval(function() {
-  Game.shimmers.forEach(function(shimmer) {
-    if (shimmer.type == "golden" || shimmer.type == "reindeer")
-    {
+setInterval(function () {
+  Game.shimmers.forEach(function (shimmer) {
+    if (shimmer.type == "golden" || shimmer.type == "reindeer") {
       shimmer.pop();
-    }else{
+    } else {
       console.log("shimmer.type = \"%s\"", shimmer.type);
     }
   })
